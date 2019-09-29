e101ccba8bee2df9f703d64fbf216bdca6b31569
BaineGames <noah.baine@gmail.com>
2019-09-29 13:27:19 -0700

    changed relative path to exact path

--- deployment/docker_deploy.sh
+++ deployment/docker_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker-compose -f ./docker/docker-compose-build.yaml push
+docker-compose -f /deployment/docker/docker-compose-build.yaml push
