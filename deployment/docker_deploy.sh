e3c6d103db967bf6f9660b6a026bb5d2b9fbc56b
BaineGames <noah.baine@gmail.com>
2019-09-29 13:38:46 -0700

    trying with docker push

--- deployment/docker_deploy.sh
+++ deployment/docker_deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
-docker-compose -f /deployment/docker/docker-compose-build.yaml push
+docker push noahross/udacity-restapi-user
+docker push noahross/udacity-restapi-feed
+docker push noahross/reverseproxy
+docker push noahross/udacity-frontend:local
